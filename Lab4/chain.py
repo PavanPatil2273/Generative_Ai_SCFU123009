838b0142c31c42e0c1ff9d52ac429a493dfcf855
Pavan Patil <pavanpatil2273@gmail.com>
2026-09-01 12:16:14 +0530

    Chain

--- Lab4/chain.py
+++ Lab4/chain.py
@@ -1,6 +1,7 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda
 from dotenv import load_dotenv
 
 
